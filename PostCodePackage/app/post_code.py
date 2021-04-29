5f76e7998d9cbde24331a6e9098a361e5c833293
avbila <abilla@spartaglobal.com>
2021-04-29 12:30:42 +0100

    Class now follows Pep 8 standards

--- PostCodePackage/app/post_code.py
+++ PostCodePackage/app/post_code.py
@@ -1,35 +1,34 @@
 import requests
 
-class PostCode():
-    def __init__(self,postcode):
-        self.postcode = postcode
-        self.result=self.getPostcodeInfo(postcode)
+
+class PostCode:
+
+    def __init__(self, postcode):
+        self.result = self.get_post_code_info(postcode)
         
-    def getPostcodeInfo(self,postcode):
+    def get_post_code_info(self, postcode):
         post_code_req = requests.get(
           f"http://api.postcodes.io/postcodes/{postcode}")
-        self.postcodeDict = (post_code_req.json()['result'])
-        result = self.postcodeDict
+        post_code_dict = (post_code_req.json()['result'])
+        result = post_code_dict
         return result
     
     def country(self):
-        nation=self.result['country']
+        nation = self.result['country']
         return nation
       
     def admin_district(self):
-        district=self.result['admin_district']
+        district = self.result['admin_district']
         return district
       
     def admin_ward(self):
-        ward=self.result['admin_ward']
+        ward = self.result['admin_ward']
         return ward
       
     def region(self):
-        reg=self.result['region']
+        reg = self.result['region']
         return reg
       
     def post_code(self):
-        postcode=self.result['postcode']
+        postcode = self.result['postcode']
         return postcode
-
-
