22d629b5c43d4b069822c84c9502834e3e7e0a4a
CIiff <clifftaku05@hotmail.co.uk>
2021-04-29 11:00:45 +0100

    pointless commit

--- PostCodePackage/app/PostCode.py
+++ PostCodePackage/app/PostCode.py
@@ -1,35 +1,34 @@
 import requests
 
+
 class PostCode:
-    def __init__(self,postcode):
+    def __init__(self, postcode):
         self.postcode = postcode
-        self.result=getPostcodeInfo(postcode)
-        
-    def getPostcodeInfo(self,postcode):
+        self.result = getPostcodeInfo(postcode)
+
+    def getPostcodeInfo(self, postcode):
         post_code_req = requests.get(
-          f"http://api.postcodes.io/postcodes/{postcode}")
+            f"http://api.postcodes.io/postcodes/{postcode}")
         self.postcodeDict = (post_code_req.json()['result'])
         result = self.postcodeDict
         return result
-    
+
     def country(self):
-        nation=self.result['country']
+        nation = self.result['country']
         return nation
-      
+
     def admin_district(self):
-        district=self.result['admin_district']
+        district = self.result['admin_district']
         return district
-      
+
     def admin_ward(self):
-        ward=self.result['admin_ward']
+        ward = self.result['admin_ward']
         return ward
-      
+
     def region(self):
-        reg=self.result['region']
+        reg = self.result['region']
         return reg
-      
+
     def post_code(self):
-        postcode=self.result['postcode']
+        postcode = self.result['postcode']
         return postcode
-
-
