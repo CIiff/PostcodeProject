989bc62fecb63f47b27a2f824a4510ff56944843
Kathryn <kdonnell997@hotmail.co.uk>
2021-04-29 10:54:10 +0100

    adding testing files

---
+++ test_postcodes.py
@@ -0,0 +1,28 @@
+from PostCode import PostCode
+import unittest
+import pytest
+
+
+class PostcodeTests(unittest.TestCase):
+
+    pc = PostCode("SE12 0NB")
+
+    def test_country(self):
+        self.assertIs(type(self.pc.country()), str)
+        self.assertEqual(self.pc.country(), "England")
+
+    def test_admin_district(self):
+        self.assertIs(type(self.pc.admin_district()), str)
+        self.assertEqual(self.pc.admin_district(), "Lewisham")
+
+    def test_admin_ward(self):
+        self.assertIs(type(self.pc.admin_ward()), str)
+        self.assertEqual(self.pc.admin_ward(), "Grove Park")
+
+    def test_region(self):
+        self.assertIs(type(self.pc.region()), str)
+        self.assertEqual(self.pc.region(), "London")
+
+    def test_postcode(self):
+        self.assertIs(type(self.pc.postcode()), str)
+        self.assertEqual(self.pc.postcode(), "SE12 0NB")
